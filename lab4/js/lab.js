d36e05f6907ce7885b23c97071ec51b1cadc01f4
Huy <mitanguy@ucsc.edu>
2022-10-17 16:51:32 -0700

    Lab 5
    Finished Lab 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,30 @@
+// Summary: Creating a website.
+// Description: Pseudo-coding for creating a basic website.
+// @link   https://mihung2001.github.io/art101/
+// @file   lab.js
+// @author Huy Nguyen and Andrew Cho.
+// @since  10.12.2922
+
+// Drawing the Human Figure:
+// 1. Get a piece of paper or anything to draw on such as a tablet, etc.
+// 2. Lightly draw the strongest direction or gesture of the pose you're going for.
+// 3. Then, lightly block in the head, torso, pelvis, legs, etc.
+//    - Don't care about the details, only focus on the bigger picture or the structure of the body like how you do.
+//    - Think of this step as the HTML and the details as css
+// 4. After blocking in the main structures, lightly sketch secondary structures such as the eyes, mouth, muscles, glasses, clothes, etc.
+// 5. Then, shade in the third structures such as details like veins, textures, creases, etc.
+// 6. After blocking in everything, render the drawing and make it clean by shading stuff in or making things or 3-dimensional.
+// 7. Finished!
+
+//Creating a webpage:
+//  1. Create a text document
+//  2. Edit the text file with a text editor.
+//  3. Type “<!DOCTYPE html> at the top of the document
+//  4. Underneath the previous step type “<html> </html>”.
+//      - Put your cursor in between the opening and closing tag and hit return/enter key on your keyboard to make the closing tag go below the opening tag.
+//  5. In between the html tags type “<body> </body>” .
+//      - Put your cursor in between the opening and closing tag and hit return/enter key on your keyboard to make the closing tag go below the opening tag.
+//  6. In between the body tags type “<p> </p>”.
+//      - In between the p tags type whatever you want.
+//  7. Close the text file and rename it “index.html”.
+//  8. View the index.html file in your browser to see if it works.
---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Author: Huy Nguyen & Andrew Cho
+ * Created : 10.17.2022
+ **/
+
+ // Define Variables
+ make = "Dodge";
+ model = "Charger";
+ color = "Black";
+ year = "2022";
+
+ // Calculate
+ age = 2022 - year;
+
+ // Output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: "+ color + "<br>");
+ document.writeln("Year: "+ year + "<br>");
+ document.writeln("Age: "+ age + " years<br>");
