b0e0968802b258ec7d28207ff584dee8b3c67b20
Huy <mitanguy@ucsc.edu>
2022-10-24 16:33:44 -0700

    Lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,25 @@
+/*
+ * Author: Huy Nguyen & Andrew Cho
+ * Created: 24 October
+ * License : Public Domain
+ */
+
+// sortUserName - a function that takes user input and sorts the letters of their sortUserName
+
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  // split string into an array
+  var nameArray = userName.split('');
+  console.log("nameArray: ", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+// output
+document.writeln("Oh hey, I've fixed your name: ", sortUserName(), "</br>");
