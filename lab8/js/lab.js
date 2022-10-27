08372aac1ac3d8a7396ed3757068e7775009d5e0
Huy <mitanguy@ucsc.edu>
2022-10-26 17:44:38 -0700

    Lab 8 Upload

---
+++ lab8/js/lab.js
@@ -0,0 +1,26 @@
+/*
+ * Author: Huy Nguyen & Andrew Cho
+ * Created: 26 October
+ * License : Public Domain
+ */
+
+ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+ console.log("My array ", numbers);
+
+ function half(x){
+   return (x / 2);
+ };
+
+//test substractOne
+console.log("What is half of 8? ", half(8));
+console.log("What is half of 94? ", half(94));
+
+var result = numbers.map(half);
+console.log("Test of halving an array: ", result);
+
+var result = numbers.map(function(x){
+  return x * 4;
+});
+
+console.log("4 times of an array: ", result);
